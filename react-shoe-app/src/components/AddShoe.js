036daa926f58e407cd9799a2bec615dbf57105ed
nishasahai <nishaabhisahai@gmail.com>
2022-02-15 22:12:11 -0500

    update shoe changes

--- react-shoe-app/src/components/AddShoe.js
+++ react-shoe-app/src/components/AddShoe.js
@@ -1,15 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import {useHistory} from "react-router";
+import {useHistory, useLocation} from "react-router";
 import moment from "moment";
 
 function AddShoe(props) {
 
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
     const history = useHistory();
+    const location = useLocation();
+
+    const shoe = location.state?.shoe;
+
+    const [brand, setBrand] = useState();
+    const [model, setModel] = useState();
+    const [typeOfShoe, setTypeOfShoe] = useState();
+    const [miles, setMiles] = useState();
+    const [cost, setCost] = useState();
+    const [startDate, setStartDate] = useState(shoe?.startDate ? new Date(shoe?.startDate) : new Date());
+    const [endDate, setEndDate] = useState(shoe?.endDate ? new Date(shoe?.endDate) : new Date());
+
+    
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -42,28 +53,29 @@ function AddShoe(props) {
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="brand" className="form-label">Brand</label>
-                    <input type="text" className="form-control" name="brand" placeholder="Brand"/>
+                    <input type="text" className="form-control" name="brand" placeholder="Brand" value={shoe?.brand} onChange={e=>setBrand(e.target.value)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="model" className="form-label">Mode</label>
-                    <input type="text" className="form-control" name="model" placeholder="Model"/>
+                    <input type="text" className="form-control" name="model" placeholder="Model" value={shoe?.model} onChange={e=>setModel(e.target.value)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="shoeType" className="form-label">Type of Shoe</label>
                     <input type="text" className="form-control" name="shoeType"
-                           placeholder="Type of Shoe(Road/Trail)"/>
+                           placeholder="Type of Shoe(Road/Trail)"
+                           value={shoe?.typeOfShoe} onChange={e=>setTypeOfShoe(e.target.value)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="miles" className="form-label">Miles Covered</label>
-                    <input type="text" className="form-control" name="miles" placeholder="Miles Covered"/>
+                    <input type="text" className="form-control" name="miles" placeholder="Miles Covered" value={shoe?.miles} onChange={e=>setMiles(e.target.value)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="cost" className="form-label">Cost</label>
-                    <input type="text" className="form-control" name="cost" placeholder="cost"/>
+                    <input type="text" className="form-control" name="cost" placeholder="cost" value={shoe?.cost} onChange={e=>setCost(e.target.value)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="startDate" className="form-label">Start Date</label>
-                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
+                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="endDate" className="form-label">End Date</label>
--- react-shoe-app/src/components/Shoe.js
+++ react-shoe-app/src/components/Shoe.js
@@ -1,36 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../images/an.jpeg"
 
 function Shoe(props) {
 
     const shoe = props.shoe;
     return (
-        <div className="card">
-            <img src={logo} className="img-thumbnail" alt="..." width="100" height="100" />
-            <div className="card-body">
-                <h5 className="card-title">Shoe Deatils</h5>
-                <div className="row">
-                    <div className="col">
-                        {shoe._id}
-                    </div>
-                    <div className="col">
-                        {shoe.brand}
-                    </div>
-                    <div className="col">
-                        {shoe.model}
-                    </div>
-                    <div className="col">
-                        {shoe.typeOfShoe}
-                    </div>
-                    <div className="col">
-                        {shoe.miles}
-                    </div>
-                    <div className="col">
-                        {shoe.cost}
+        <Link to={{
+            pathname: "/AddShoe",
+            state: {shoe}
+        }}>
+            <div className="card">
+                <img src={logo} className="img-thumbnail" alt="..." width="100" height="100" />
+                <div className="card-body">
+                    <h5 className="card-title">Shoe Deatils</h5>
+                    <div className="row">
+                        <div className="col">
+                            {shoe._id}
+                        </div>
+                        <div className="col">
+                            {shoe.brand}
+                        </div>
+                        <div className="col">
+                            {shoe.model}
+                        </div>
+                        <div className="col">
+                            {shoe.typeOfShoe}
+                        </div>
+                        <div className="col">
+                            {shoe.miles}
+                        </div>
+                        <div className="col">
+                            {shoe.cost}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
+
     );
 }
 
